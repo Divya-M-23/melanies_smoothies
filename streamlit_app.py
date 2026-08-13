16fda672e0263e714fef5e45119455c998b077fc
Divya-M-23 <mdivya0923@gmail.com>
2026-08-13 15:07:31 +0530

    Comment out pd_df display and stop calls
    Comment out dataframe display and stop execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # CONVERT THE SNOWPARK DATAFRAME TO A PANDAS DATAFRAME SO WE CAN USE THE LOC FUNCTION
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 
@@ -83,6 +83,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
